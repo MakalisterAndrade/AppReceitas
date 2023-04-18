34b3437c1f933b295b1b646538d708e434800e23
MakalisterAndrade <makalister.andrade@outlook.com>
2023-04-18 18:35:03 -0400

    p

---
+++ lib/models/meal.dart
@@ -0,0 +1,19 @@
+class Meal {
+  final String id;
+  final List<String> categories;
+  final String title;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+
+  const Meal({
+    required this.id,
+    required this.categories,
+    required this.title,
+    required this.imageUrl,
+    required this.ingredients,
+    required this.steps,
+    required this.duration,
+  });
+}
